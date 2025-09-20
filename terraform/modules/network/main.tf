534b9690d121ecf6b0097ae10aef4b829630e75f
fabiobritopinto@gmail.com <fabiobritopinto@gmail.com>
2025-09-19 22:52:01 -0300

    Nova Estrutura

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,14 @@
+# Azure VNET
+
+# Azure VNET Subnet
+resource "aws_subnet" "public_subnets" {
+  count                   = var.vpc_public_subnet_count
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.app.id
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.naming_prefix}-subnet-${count.index}"
+  })
+}
